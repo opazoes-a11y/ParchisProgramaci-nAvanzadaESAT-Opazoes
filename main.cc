2281efef1778ebeb399aa29dfd5b01a49d886149
Joaquin Opazo <opazoes@esat-alumni.com>
2025-10-30 11:11:18 +0100

    Commented lines with errors to check compiling

--- main.cc
+++ main.cc
@@ -4,13 +4,13 @@
 
 int main(){
   jop_parcheesi myp;
-  const IPlayer* players[4]={myp.players};
-  Game g{myp,players};
-
+  //const IPlayer* players[4]={myp.players};
+  //Game g{myp,players};
+/*
   while(g.IsGameOver() == IParcheesi::Color::None){
     g.PlayTurn();
   }
-
+*/
   return 0;
 }
 
